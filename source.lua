b326cac14160bf5bf4e8710e8da3c577ab439839
Vitor Ramos <ramossvitor@gmail.com>
2021-05-30 00:53:42 -0300

    Update source.lua

--- source.lua
+++ source.lua
@@ -114,3 +114,91 @@ text_a = "Oi tudo bem?"
 text_b = "Como ta?"
 
 print(text_a .. " " .. text_b)
+
+print("=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=")
+
+-- More packages
+print(string.len("hello"))
+print(string.sub("hello", 1, 3))
+
+texto_1 = "TestaNdo"
+print(string.upper(texto_1))
+print(string.lower(texto_1))
+capitalizando = string.sub(texto_1, 1, 1)
+capitalizando_f = string.sub(texto_1, 2, #texto_1)
+capitalizada = string.upper(capitalizando) .. string.lower(capitalizando_f)
+print(capitalizada)
+print("=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=")
+
+print(string.rep("A", 592)) -- pode ser usado para margem também, exemplo a baixo
+print("=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=")
+
+margemtexto = string.rep(" ", 2)
+print(margemtexto .. capitalizada)
+print("=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=")
+
+-- reveter texto
+print(string.reverse("ABCD"))
+
+print("=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=")
+
+-- procurando coisas dentro de textos
+textfind = "ramossvitor@gmail.com"
+
+print(string.find(textfind, "yahoo.com"))
+print(string.find(textfind, "outlook.com"))
+print(string.find(textfind, "gmail.com"))
+
+findfrom, findto = string.find(textfind, "gmail.com")
+print(string.sub(textfind, findfrom, findto))
+print("=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=")
+print(string.match(textfind, "gmail.com"))
+print("=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=")
+place_1 = string.find(textfind, "@")
+if place_1 == nil then
+    print("Email invalido!")
+else
+    provedor_1 = string.sub(textfind, place_1 + 1, #textfind)
+    print("Provedor: " .. provedor_1)
+end
+print("=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=")
+texto_2 = "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Temporibus maiores quaerat corporis asperiores nesciunt quasi dolor aliquam illo porro. Necessitatibus commodi blanditiis quia alias cupiditate doloribus ea itaque omnis at!"
+result_5 = string.gsub(texto_2, "dolor", "ahdhj")
+print(result_5)
+print("=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=")
+texto_3 = "Lorem, ipsum dolor dolor dolor consectetur adipisicing elit. Temporibus maiores quaerat corporis asperiores nesciunt quasi dolor aliquam illo porro. Necessitatibus commodi blanditiis quia alias cupiditate doloribus ea itaque omnis at!"
+result_6 = string.gsub(texto_3, "dolor", "daqjsdja", 1)
+print(result_6)
+print("=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=")
+
+-- Listas
+
+lista_1 = {4, 6, 7, 9}
+print(lista_1[3])
+
+-- alterar valores da lista
+lista_1[3] = 10
+print(lista_1[3])
+
+-- Adicionando valores na lista
+lista_1[5] = 123
+print(lista_1[5])
+
+-- Inserindo sem escolher a posição
+table.insert(lista_1, 71)
+-- Ele irá colocar no proximo local disponivel da lista
+
+print("=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=")
+
+numero_1 = 7
+
+lista_2 = {4, 6, 8, numero_1, 12}
+print(lista_2[4])
+
+print("=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=")
+
+-- Representação de listas
+
+for position in pairs(lista_1) do
+    print(position .. ": " .. lista_1[position])
+end
